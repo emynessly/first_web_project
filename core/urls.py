a77616f47f07348684c5845651929b18b8a5af03
Veronika Pihtyar <veronika17092005@mail.ru>
2026-05-14 03:11:30 +0300

    Added templates and second page (Lesson 4)

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
+from django.views.generic import RedirectView
 
 from core.views import index
+from core.views import top
 
 app_name = 'core'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('top/', top, name='top'),
+    path('top.html', RedirectView.as_view(url='/top/', permanent=True), name='top_redirect'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
 
+def top(request):
+    top = [
+            {"title": "1984", "author": "Джордж Оруэлл"},
+            {"title": "Мы", "author": "Евгений Замятин"},
+            {"title": "451 градус по Фаренгейту", "author": "Рэй Брэдбери"},
+        ]
+    context = {
+        'top': top,
+    }
+    return render(request, 'core/top.html', context)
+
 def index(request):
-    return render(request, 'core/index.html')
+    context = {
+        'title': 'Добро пожаловать в Мир книг!',
+        'welcome_text': '<i>«Мир книг»</i> — каталог книг для читателей всех возрастов. '
+            'Здесь собраны произведения разных жанров: от классики до современной прозы. '
+            'Добавляйте книги в избранное, оставляйте отзывы и находите новые истории для души.'
+    }
+    return render(request, 'core/index.html', context)
